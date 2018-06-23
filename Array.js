334e5baa3976a7d08951cb3381c4d7602eaa550b
javajaket <kt5161497@gmail.com>
2018-06-23 14:18:49 +0900

    Array.js edit

--- Array.js
+++ Array.js
@@ -45,3 +45,83 @@ console.log(myVar1);
 console.log(myVar2);
 console.log(myVar3);
 console.log(myVar4);
+
+/*Array.prototype.sort
+- 배열의 내용을 적절하게 정렬,원본 배열을 직접 변경하며 정렬된 배열을 반환. 반환된 배열은 복사본이 아닌 원본 배열
+  return 0;    //정상 종료
+  return -1;   //에러 발생
+  return 1이상; //정상 종료 되었으나 다른 무엇인가 있음을 나타냄 
+  return -2같은 숫자; //에러 발생했으나 구체적으로 무엇이다를 나타냄
+*/
+var items = [
+  {name: 'Edward', value:21},
+  {name: 'Sharpe', value:37},
+  {name: 'And', value:45},
+  {name: 'The', value:-12},
+  {name: 'Magnetic'},
+  {name: 'Zeros', value:37}
+];
+
+//value 기준으로 정렬
+items.sort(function(a,b){
+  if(a.value > b.value){
+    return 1;
+  }
+  if(a.value < b.value){
+    return -1;
+  }
+  return 0;
+})
+console.log(items);
+
+//name 기준으로 정렬
+items.sort(function(a,b){
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  if(nameA < nameB){
+    return -1;
+  }
+  if(nameA > nameB){
+    return 1;
+  }
+  return 0;
+})
+
+console.log(items);
+
+var points = [40, 100, 1, 5, 2, 25, 10];
+
+// 숫자 배열 오름차순 정렬
+// compareFunction의 반환값이 0보다 작은 경우, a를 우선한다.
+points.sort(function (a, b) { return a - b; });
+console.log(points); // [ 1, 2, 5, 10, 25, 40, 100 ]
+
+// 숫자 배열에서 최소값 취득
+console.log(points[0]); // 1
+
+// 숫자 배열 내림차순 정렬
+// compareFunction의 반환값이 0보다 큰 경우, b를 우선한다.
+points.sort(function (a, b) { return b - a; });
+console.log(points); // [ 100, 40, 25, 10, 5, 2, 1 ]
+
+// 숫자 배열에서 최대값 취득
+console.log(points[0]); // 100
+
+var todos = [
+  { id: 4, content: 'JavaScript'},
+  { id: 1, content: 'HTML'},
+  { id: 2, content: 'CSS'}
+];
+//id를 기준으로 정렬
+todos.sort(function(a,b){
+  return (a.id > b.id) ? 1 : (a.id < b.id) ? -1 : 0;
+});
+console.log(todos);
+
+//content를 기준으로 정렬
+todos.sort(function(a,b){
+  return (a.content > b.content) ? 1: (a.content < b.content) ? -1 : 0;
+});
+console.log(todos);
+
+
